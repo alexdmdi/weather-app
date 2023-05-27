51f8e65137c69379d90c39e93fb2dfcc1ddcf2da
alexdmdi <alexdmdi@outlook.com>
2023-05-26 21:36:05 -0400

    themes and accordion added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import NavBar from './components/Navbar'
+import FutureForecast from './components/FutureForecast';
 
 function App() {
 
   return (
     <>
+      
+      
       <div className="container">
-        <div className="row border border-black border-3">
-          <div className="col-xl-1 border border-black border-3"></div>
+        <NavBar/>
+        <div className="row border rounded ms-0 me-0 mb-5 contentBox ">
+          <div className="col-xl-1 t"></div>
 
-          <div className="col text-center border border-black border-3">
-          hi
+          <div className="col pt-3 pb-5 ">
+            <h2 className="display-3">18c</h2>
+            <h3 className="fs-4">Toronto, Ontario</h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nesciunt 
+          quia reiciendis nihil neque ex, omnis maxime, tempora similique fuga aliquid quibusdam aperiam 
+          hic ullam delectus enim alias aliquam, nisi facilis unde consequatur voluptatum quae nobis? Consequuntur,
+           
           </div>
 
-          <div className="col-xl-1 border border-black border-3"></div>
+          <div className="col-xl-1"></div>
 
+        </div>
+      
+        <div className="row border rounded ms-0 me-0">
+          <FutureForecast/>
         </div>
       
       </div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import styles from "../App.css"
+
+function NavBar() {
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg mb-5 rounded mt-3 navCustomized">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="https://www.alexdiamandi.com">Weather Forecast App</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 me-5 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="https://www.alexdiamandi.com">Creator Home page</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="https://github.com/alexdmdi/weather-app">Source Code</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+
+    )
+
+}
+
+export default NavBar
