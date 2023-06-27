875e7fad796d1b32badfe8da19fe7d4fb04f0202
alexdmdi <alexdmdi@outlook.com>
2023-06-27 14:20:47 -0400

    updated comments

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
-import styles from '../App.css';
 import config from "../config"
 import { Location } from "./types";
 
 interface WeatherProps {
   selectedLocation: Location | null | undefined;  //specifies that selecedLocation may be null or undefined, not just of type Location
-  
-  updateForecastData: (data: object) => void; //specifies that updateWeatherData is a function that takes an object as an argument and returns nothing
+  updateForecastData: (data: object) => void; //specifies that updateWeatherData is a function that takes an object argument, and returns nothing
 }
 
 interface CurrentLocation {
@@ -18,7 +16,7 @@ interface CurrentLocation {
 }
 
 const Weather = ({ selectedLocation, updateForecastData }: WeatherProps) => {
-  const [forecastData, setForecastData] = useState<any>(null); //forecastData is set here, but only so it can be used in futureforecast.tsx
+  const [forecastData, setForecastData] = useState<any>(null); //forecastData is set here, but so it can be ultimately used in futureforecast.tsx
   const [currentWeather, setCurrentWeather] = useState<any>(null);
   const [currWeatherIcon, setCurrWeatherIcon] = useState<string>("");
   const [currLocation, setCurrLocation] = useState<CurrentLocation | null>(null);
@@ -34,12 +32,12 @@ const Weather = ({ selectedLocation, updateForecastData }: WeatherProps) => {
       console.log('Current Weather data:', currentWeatherData);
       setCurrentWeather(currentWeatherData); 
 
-      //display correct icon for current weather
+      //Set correct icon for current weather based on the code provided in the response
       const currIconCode = currentWeatherData.weather[0].icon;
       const currIconURL: string = `https://openweathermap.org/img/wn/${currIconCode}@2x.png`;
       setCurrWeatherIcon(currIconURL);
 
-      // Fetch forecast data
+      //Fetch forecast data
       const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?${locationId ? `id=${locationId}` : `lat=${latitude}&lon=${longitude}`}&appid=${config.apiKey}&units=metric`);
       if (!forecastResponse.ok) {
         throw new Error('Forecast data request failed');
@@ -54,15 +52,14 @@ const Weather = ({ selectedLocation, updateForecastData }: WeatherProps) => {
     }
   };
 
-  
+  //uses ipApi.co to get users approximate coordinates, so it may be used to fetch current weather and forecast data for their location, on mount (page load)
   useEffect(() => {
     const getUserLocation = async () => {
       try {
         const response = await fetch(`https://ipapi.co/json`);
         const data = await response.json();
         console.log(data);
-        const currUserData = { 
-          lat: data.latitude, lon: data.longitude, city: data.city, regionCode: data.region_code, countryName: data.country_name };
+        const currUserData = { lat: data.latitude, lon: data.longitude, city: data.city, regionCode: data.region_code, countryName: data.country_name };
         return currUserData;
       } 
       catch (error) {
@@ -93,8 +90,7 @@ const Weather = ({ selectedLocation, updateForecastData }: WeatherProps) => {
   return(
       <div className="row border rounded ms-0 me-0 mb-5 contentBox ">
         
-        
-        <div className="col-5 d-flex align-items-center justify-content-center border-end border-light">
+        <div className="col-5 d-flex align-items-center justify-content-center border-end border-light weatherSummary">
           
           {currentWeather ? (
             <div className = "text-center">
@@ -102,8 +98,8 @@ const Weather = ({ selectedLocation, updateForecastData }: WeatherProps) => {
                 {Math.round(currentWeather.main.temp)}<sup>°C</sup> 
               </h2>
               <p>{`Feels like ${Math.round(currentWeather.main.feels_like)}`}°C</p>
-              <h3 className="fs-5 pt-2 text-info">
-                {`${selectedLocation? `${selectedLocation?.name}, ${selectedLocation?.country}`   :    `${currLocation? `${currLocation.city} ${currLocation.regionCode? `${currLocation.regionCode}` : ""}, ${currLocation.countryName}` : `${'null'}` }` }`}
+              <h3 className="fs-5 pt-2 text-info locationText">
+                {`${selectedLocation? `${selectedLocation?.name}, ${selectedLocation?.country}`  :  `${currLocation? `${currLocation.city} ${currLocation.regionCode? `${currLocation.regionCode}` : ""}, ${currLocation.countryName}` : `${'null'}` }` }`}
               </h3>
               {`Humidity: ${currentWeather.main.humidity}%`} 
             </div>
