a9178f42f9dc4ddd47299e10ea595bd617bcabf5
alexdmdi <alexdmdi@outlook.com>
2023-06-28 18:25:29 -0400

    theme functionality and bootstrap grid changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import './App.css'
+// import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import NightCSS from './components/NightCSS'
+import DayCSS from './components/DayCSS'
 
-import React, { useState, Suspense } from "react";
+import React, { useState, Suspense, useEffect } from "react";
 
 import NavBar from './components/Navbar'
 import FutureForecast from './components/FutureForecast';
@@ -17,13 +19,21 @@ function App() {
   const [filteredLocations, setFilteredLocations] = useState<Location[]>([]);
   const [selectedLocation, setSelectedLocation] = useState<Location | null>();
   const [forecastData, setForecastData] = useState<any>(null); //gets passed to futureforecast
+  const [dayThemeBoolean, setDayThemeBoolean] = useState<boolean>(); //true: DayCSS used, false: NightCSS used
 
+  const updateTheme = (booleanValue: boolean) => {
+    setDayThemeBoolean(booleanValue);
+    console.log(`current weather data in app.tsx has been set to: ${(booleanValue==true)? "DayCSS" : "NightCSS"}`)
+  }
   const updateForecastData = (forecastData: object) => {
     setForecastData(forecastData);
   }
 
   return (
     <>
+        <style>
+          {(dayThemeBoolean === true)? DayCSS : NightCSS}
+        </style>
         <div className="container">
           <NavBar />
           <h1 className="display-5 fw-normal text-center mb-2 ">
@@ -33,10 +43,10 @@ function App() {
             </div>
           </h1>
 
-          <div className="row text-center">
-            <div className="col-xl-3 col-lg-3 col-md-2"></div>
+          <div className="row text-center justify-content-center">
+            <div className="col"></div>
             
-            <div className="col pt-3 mb-4 search-bar-container p-0">
+            <div className="col-10 col-lg-5 col-sm-8 pt-3 mb-4 search-bar-container p-0">
               <div className="fs-4 contentBox p-1 rounded">
                 <div className="search-container">
                     <Suspense fallback={<div className="fs-5">Loading...</div>}>
@@ -46,32 +56,33 @@ function App() {
               </div>
             </div>
 
-            <div className="col-xl-3 col-lg-3 col-md-2"></div>
+            <div className="col"></div>
           
           </div>
 
-          <div className="row">
+          <div className="row justify-content-center">
             
-            <div className="col-xl-2 col-lg-1 col-md-1">   </div>
+            <div className="col  col-md-1">   </div>
             
-            <div className="col-xl-8 col-lg-10 col-md-10">
-              <Weather selectedLocation={selectedLocation} updateForecastData={updateForecastData}/>
+            <div className="col-xl-7 col-lg-8 col-md-10">
+              <Weather selectedLocation={selectedLocation} updateForecastData={updateForecastData} updateTheme={updateTheme}/>
 
               <div className="row rounded ms-0 me-0 mb-5">
                 <FutureForecast forecastData={forecastData} />
               </div>    
             </div>
             
-            <div className="col-xl-2 col-lg-1 col-md-1"></div>
+            <div className="col  col-md-1"></div>
           </div>
           
         </div>
 
       
-
     </>
   );
+
 }
+
 
 export default App
 
