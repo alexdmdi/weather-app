49997e3e19faae59abd8495d43b90928a6f66547
alexdmdi <alexdmdi@outlook.com>
2023-07-02 12:18:50 -0400

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/weather-app/',
-})
+
+export default defineConfig(function ({ mode }) {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react()],
+    base: '/weather-app/',
+    define: {
+      'import.meta.env.VITE_APIKEY': JSON.stringify(isProduction ? process.env.VITE_APIKEY : import.meta.env.VITE_APIKEY),
+    },
+  };
+});
+
