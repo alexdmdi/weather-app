faa1753d86e13edf248232e32144d162d3751eb8
alexdmdi <alexdmdi@outlook.com>
2023-07-02 13:53:04 -0400

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+require('dotenv').config();
 
 export default defineConfig(function ({ mode }) {
   const isProduction = mode === 'production';
